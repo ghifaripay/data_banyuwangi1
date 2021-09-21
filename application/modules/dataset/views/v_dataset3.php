1180159e1a7584461eb592887524c082747d40ff
statistikdansandi <statisdansandi@gmail.com>
2021-09-21 14:49:07 +0700

    tes

--- application/modules/dataset/views/v_dataset3.php
+++ application/modules/dataset/views/v_dataset3.php
@@ -22,7 +22,7 @@
             <div class="collapse navbar-collapse mean-menu" id="navbarSupportedContent">
                 <ul class="navbar-nav">
                     <li class="nav-item"><a href="<?php echo base_url()?>b_data" class="nav-link">Home</a></li>
-                    <li class="nav-item"><a href="<?php echo base_url()?>dataset" class="nav-link">Dataset</a></li>
+                    <li class="nav-item"><a href="<?php echo base_url()?>dataset" class="nav-link">Data</a></li>
                     <!-- <li class="nav-item"> <a href="#!" class="nav-link">Dataset<i class="fas fa-chevron-down"></i></a>
                         <ul class="dropdown-menu">
                             <li class="nav-item"><a href="service.html" class="nav-link">BPS</a></li>
@@ -47,9 +47,14 @@
       <div class="col-lg-12 col-md-12 col-sm-12 col-12">
           <div class="blog-wrapper-blog-item">
               <div class="blog-wedget box-shadow">
+                <!-- <div class="post-img">
+                    <a href="#">
+                        <img src="<?php echo base_url() ?>assets/frontend2/img\blog\recent-post\2.png" alt="img">
+                    </a>
+                </div> -->
                   <div class="blog-wedget-text">
                       <div class="blog-wedget-heading">
-                          <h2><a href="blog-detail.html">Data Kabupaten Banyuwangi</a></h2>
+                          <h2><a href="blog-detail.html">Dataset Kabupaten Banyuwangi</a></h2>
                       </div>
                       <div class="blog-wedget-author">
                           <div class="blog-author-view">
@@ -60,6 +65,7 @@
                                   </li>
                                   <li><a href="#!"><i class="icofont-book-alt"></i> Kecamatan</a>
                                   </li>
+
                               </ul>
                           </div>
                           <div class="blog-author-date">
@@ -74,78 +80,174 @@
                           <p>kumpulan data Dalam kasus data tabular, kumpulan data sesuai dengan satu atau beberapa tabel database, di mana setiap kolom tabel mewakili variabel tertentu, dan setiap baris sesuai dengan rekaman tertentu dari kumpulan data yang dimaksud.
                           </p>
                       </div>
-                  </div><br>
 
+                  </div>
               </div>
-
           </div>
-
       </div>
-
     </div>
+
   </div>
 </section>
+
 
 
     <section id="blog-main-area" >
         <div class="container">
             <div class="row">
               <div class="col-lg-3 col-md-12 col-sm-12 col-12">
-                <div class="check-heading">
-                  <h4>Filter Data</h4>
-                </div>
                   <div class="left-head-blog right-side">
                       <div class="left-blog-page box-shadow">
+                          <!-- search option start -->
                           <form action="#">
                               <div class="blog-search-option">
-                                  <input type="text" placeholder="Cari Data">
+                                  <input type="text" placeholder="Search...">
                                   <button class="button" type="submit"> <i class="fa fa-search"></i>
                                   </button>
                               </div>
                           </form>
+                          <!-- search option end -->
+                      </div>
+                      <div class="left-blog-page box-shadow">
+                          <div class="left-blog">
+                              <h4>Urusan</h4>
+                              <ul>
+                                  <li><a href="#">Pendidikan</a><span>12</span>
+                                  </li>
+                                  <li><a href="#">Kesehatan </a><span>17</span>
+                                  </li>
+                                  <li><a href="#">Ekonomi</a><span>07</span>
+                                  </li>
+                                  <li><a href="#">Kependudukan</a><span>21</span>
+                                  </li>
+                                  <li><a href="#">Pariwisata</a><span>14</span>
+                                  </li>
+                                  <!-- <li><a href="#">development</a><span>10</span>
+                                  </li>
+                                  <li><a href="#">Design</a><span>15</span>
+                                  </li> -->
+                              </ul>
+                          </div>
+                      </div>
+                      <div class="left-blog-page box-shadow">
+                          <!-- recent start -->
+                          <div class="left-blog">
+                              <h4>SKPD</h4>
+                              <div class="recent-post">
+                                  <!-- start single post -->
+                                  <!-- <div class="recent-single-post">
+                                      <div class="post-img">
+                                          <a href="#">
+                                              <img src="<?php echo base_url() ?>assets/frontend2/img\blog\recent-post\1.png" alt="img">
+                                          </a>
+                                      </div>
+                                      <div class="pst-content">
+                                          <p><a href="#">We offer professional Consultant services.</a>
+                                          </p> <span class="date-type">
+                                              26 Jan / 2018
+                                          </span>
+                                      </div>
+                                  </div> -->
+                                  <!-- End single post -->
+                                  <!-- start single post -->
+                                  <!-- <div class="recent-single-post">
+                                      <div class="post-img">
+                                          <a href="#">
+                                              <img src="<?php echo base_url() ?>assets/frontend2/img\blog\recent-post\2.png" alt="img">
+                                          </a>
+                                      </div>
+                                      <div class="pst-content">
+                                          <p><a href="#">Montril is an firm and general group.</a>
+                                          </p> <span class="date-type">
+                                              20 June / 2018
+                                          </span>
+                                      </div>
+                                  </div> -->
+                                  <!-- End single post -->
+                                  <!-- start single post -->
+                                  <!-- <div class="recent-single-post">
+                                      <div class="post-img">
+                                          <a href="#">
+                                              <img src="<?php echo base_url() ?>assets/frontend2/img\blog\recent-post\3.png" alt="img">
+                                          </a>
+                                      </div>
+                                      <div class="pst-content">
+                                          <p><a href="#">Business man always think positive.</a>
+                                          </p> <span class="date-type">
+                                              26 Feb / 2018
+                                          </span>
+                                      </div>
+                                  </div> -->
+                                  <!-- End single post -->
+                                  <!-- start single post -->
+                                  <!-- <div class="recent-single-post">
+                                      <div class="post-img">
+                                          <a href="#">
+                                              <img src="<?php echo base_url() ?>assets/frontend2/img\blog\recent-post\1.png" alt="img">
+                                          </a>
+                                      </div>
+                                      <div class="pst-content">
+                                          <p><a href="#">Man can change business policy all time.</a>
+                                          </p> <span class="date-type">
+                                              13 Nov / 2018
+                                          </span>
+                                      </div>
+                                  </div> -->
+                                  <!-- End single post -->
+                              </div>
+                          </div>
+                          <!-- recent end -->
+                      </div>
+                      <div class="left-blog-page box-shadow">
+                          <div class="left-tags blog-tags">
+                              <div class="popular-tag left-side-tags left-blog">
+                                  <h4>Kecamatan</h4>
+                                  <!-- <ul>
+                                      <li><a href="#">Business</a>
+                                      </li>
+                                      <li><a href="#">Agency </a>
+                                      </li>
+                                      <li><a href="#">Media</a>
+                                      </li>
+                                      <li><a href="#">Social</a>
+                                      </li>
+                                      <li><a href="#">Photoshop</a>
+                                      </li>
+                                      <li><a href="#">Seo</a>
+                                      </li>
+                                      <li><a href="#">development</a>
+                                      </li>
+                                      <li><a href="#">Search</a>
+                                      </li>
+                                      <li><a href="#">Design</a>
+                                      </li>
+                                  </ul> -->
+                              </div>
+                          </div>
                       </div>
                   </div>
-                  <div class="">
-                      <div class="">
-                    <div class="form-group">
-                        <!-- <label for="city">State/City</label> -->
-                        <select class="form-control first_null" id="city">
-                            <option value="">Pilih Kategori</option>
-                            <option value="AX">Badan Pusat Statistik</option>
-                            <option value="AF">Sektoral</option>
-                            <option value="AF">Kecamatan</option>
-                        </select>
-                    </div>
-                  </div>
-                </div>
-                <div class="">
-                    <div class="">
-                  <div class="form-group">
-                      <!-- <label for="zip">State/City</label> -->
-                      <select class="form-control first_null" id="city">
-                          <option value="">Urusan</option>
-                          <option value="AX">Pendidikan</option>
-                          <option value="AF">Kesehatan</option>
-                          <option value="AF">Kependudukan</option>
-                      </select>
-                  </div>
-                </div>
-              </div>
               </div>
               <div class="col-lg-9 col-md-12 col-sm-12 col-12">
                     <div class="blog-wrapper-blog-item">
                         <div class="blog-wedget box-shadow">
-
+                            <!-- <div class="blog-wedget-img">
+                                <a href="blog-detail.html">
+                                    <img src="<?php echo base_url()?>assets/frontend2/img\blog\post1.jpg" alt="img">
+                                </a>
+                            </div> -->
                             <div class="blog-wedget-text">
-
+                              <div class="post-img">
+                                  <a href="#">
+                                      <img src="<?php echo base_url() ?>assets/frontend2/img\blog\recent-post\1.png" alt="img">
+                                  </a>
+                              </div>
                                 <div class="blog-wedget-heading">
                                     <h4><a href="<?php echo base_url() ?>dataset/dtl_i">Angka Parsitipasi Kasar PAUD</a></h4>
                                 </div>
                                 <div class="blog-wedget-author">
                                     <div class="blog-author-view">
                                         <ul>
-                                            <li>
-                                              <a href="#!"><i class="icofont-book-alt"></i> Pendidikan</a>
+                                            <li><a href="#!"><i class="icofont-book-alt"></i> Pendidikan</a>
                                             </li>
                                             <li><a href="#!"><i class="icofont-building-alt"></i> Dinas Pendidikan</a>
                                             </li>
@@ -159,14 +261,20 @@
                                         </ul>
                                     </div>
                                 </div>
-
-
-                    </div>
-
+                                <!-- <div class="blog-wedget-paragraph">
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel mollitia eos a.
+                                        Laborum reprehenderit animi magnam ipsam tenetur natus, similique earum cum, ut
+                                        fugit distinctio dolores ad corporis doloribus consectetur.
+                                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate molestiae
+                                        quo reprehenderit nobis mollitia et in ipsa tenetur atque error iure, magni
+                                        possimus adipisci omnis, similique sapiente excepturi aliquam
+                                        laboriosam.
+                                    </p>
+                                </div>
+                                <div class="blog-wedget-button"> <a href="blog-detail.html" class="btn btn-theme">Read More..</a>
+                                </div> -->
                             </div>
-
                         </div>
-
                         <div class="blog-wedget box-shadow">
                             <!-- <div class="blog-wedget-img">
                                 <a href="blog-detail.html">
@@ -175,7 +283,11 @@
                             </div> -->
 
                             <div class="blog-wedget-text">
-
+                              <div class="post-img">
+                                  <a href="#">
+                                      <img src="<?php echo base_url() ?>assets/frontend2/img\blog\recent-post\1.png" alt="img">
+                                  </a>
+                              </div>
                                 <div class="blog-wedget-heading">
                                     <h4><a href="<?php echo base_url() ?>dataset/dtl_v">Jumlah Sekolah Dasar</a></h4>
                                 </div>
@@ -217,7 +329,11 @@
                                 </a>
                             </div> -->
                             <div class="blog-wedget-text">
-
+                              <div class="post-img">
+                                  <a href="#">
+                                      <img src="<?php echo base_url() ?>assets/frontend2/img\blog\recent-post\3.png" alt="img">
+                                  </a>
+                              </div>
                                 <div class="blog-wedget-heading">
                                     <h4><a href="<?php echo base_url() ?>dataset/detail_variabel">Jumlah Siswa</a></h4>
                                 </div>
@@ -259,7 +375,11 @@
                                 </a>
                             </div> -->
                             <div class="blog-wedget-text">
-
+                              <div class="post-img">
+                                  <a href="#">
+                                      <img src="<?php echo base_url() ?>assets/frontend2/img\blog\recent-post\2.png" alt="img">
+                                  </a>
+                              </div>
                                 <div class="blog-wedget-heading">
                                     <h4><a href="<?php echo base_url() ?>dataset/detail">Angka Parsitipasi Kasar SMA</a></h4>
                                 </div>
